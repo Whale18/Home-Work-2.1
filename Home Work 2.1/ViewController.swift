abe310e1fc588999a646451c5e6437dd39712ff0
Whale18 <86872488+Whale18@users.noreply.github.com>
2022-06-15 13:34:47 +0300

    Initial Commit

---
+++ Home Work 2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 2.1
+//
+//  Created by Никита Мещеряков on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
